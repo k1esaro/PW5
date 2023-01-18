c1608f65aa75a9ba6c4916358e096894ba0449c9
Kiesaro <freestylerspb@gmail.com>
2023-01-18 17:11:07 +0300

    Example 34

---
+++ Example34/Program.cs
@@ -0,0 +1,31 @@
+﻿void input(int [] array)
+{
+    for (int i = 0; i< array.Length ; i++)
+    {
+        array[i] = new Random().Next(100 ,999);
+    }
+}
+int release(int [] array)
+{
+    int result = 0;
+    for (int i = 0; i< array.Length ; i++)
+    {
+        if(array[i]%2==0)
+        {
+            result+=1;
+        }
+    }
+    return result;
+}
+void output(int [] array)
+{
+        Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+input(array);
+output(array);
+Console.Write("Колл-во четных чисел равняется = ");
+Console.Write(release(array));
